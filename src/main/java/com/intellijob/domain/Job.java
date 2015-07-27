6680caf2f3a3ad73e8ae70551f1f8e684ead06be
SergejMeister <sergej_meister@gmx.net>
2015-07-27 18:23:22 +0200

    change property reference to DBRef

--- src/main/java/com/intellijob/domain/Job.java
+++ src/main/java/com/intellijob/domain/Job.java
@@ -17,8 +17,8 @@
 package com.intellijob.domain;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Reference;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -45,7 +45,7 @@ public class Job {
     /**
      * Reference to jobLink.
      */
-    @Reference
+    @DBRef
     private JobLink jobLink;
 
     /**
