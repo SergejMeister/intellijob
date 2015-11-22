217674d0deac77512d7a58bb2faaf1830807e62d
SergejMeister <sergej_meister@gmx.net>
2015-11-22 20:43:03 +0100

    bug-fix disable skill rating change in readonly mode

--- src/main/webapp/js/app/directives/star-rating.drt.js
+++ src/main/webapp/js/app/directives/star-rating.drt.js
@@ -13,44 +13,48 @@
  *  See the License for the specific language governing permissions and
  * limitations under the License.
  */
-intelliJob.directive('starRating', function() {
-                    return {
-                        restrict: 'A',
-                        template: '<ul class="rating">'
-                        + '	<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">'
-                        + '\u2605'
-                        + '</li>'
-                        + '</ul>',
-                        scope: {
-                            ratingValue: '=',
-                            max: '=',
-                            onRatingSelected: '&'
-                        },
-                        link: function (scope, elem, attrs) {
-                            var updateStars = function () {
-                                scope.stars = [];
-                                for (var i = 0; i < scope.max; i++) {
-                                    scope.stars.push({
-                                        filled: i < scope.ratingValue
-                                    });
-                                }
-                            };
-
-                            scope.toggle = function (index) {
-                                scope.ratingValue = index + 1;
-                                scope.onRatingSelected({
-                                    rating: index + 1
-                                });
-                            };
-
-                            scope.$watch('ratingValue',
-                                    function (oldVal, newVal) {
-                                        if (newVal) {
-                                            updateStars();
-                                        }
-                                    }
-                            );
+intelliJob.directive('starRating', function () {
+            return {
+                restrict: 'A',
+                template: '<ul class="rating">'
+                + '	<li ng-repeat="star in stars" ng-class="star" ng-click="starDisabled || toggle($index)">'
+                + '\u2605'
+                + '</li>'
+                + '</ul>',
+                scope: {
+                    ratingValue: '=',
+                    max: '=',
+                    onRatingSelected: '&',
+                    starDisabled: '='
+                },
+                link: function (scope, elem, attrs) {
+                    if (!scope.starDisabled) {
+                        scope.starDisabled = false;
+                    }
+                    var updateStars = function () {
+                        scope.stars = [];
+                        for (var i = 0; i < scope.max; i++) {
+                            scope.stars.push({
+                                filled: i < scope.ratingValue
+                            });
                         }
                     };
+
+                    scope.toggle = function (index) {
+                        scope.ratingValue = index + 1;
+                        scope.onRatingSelected({
+                            rating: index + 1
+                        });
+                    };
+
+                    scope.$watch('ratingValue',
+                            function (oldVal, newVal) {
+                                if (newVal) {
+                                    updateStars();
+                                }
+                            }
+                    );
                 }
-        );
+            };
+        }
+);
